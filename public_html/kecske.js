72057232f9927d31ca232ca8875bbadb8c850296
Lala <dlabanc@gmail.com>
2021-04-19 18:58:19 +0200

    Végleges kész játék - MEGCSINÁLTUKgit add .!

--- public_html/kecske.js
+++ public_html/kecske.js
@@ -13,15 +13,20 @@ var kepObj3 = {
     cim: "farkas"
 };
 
-var kepObjTomb = [kepObj1, kepObj2, kepObj3];
+var KepObjTomb = [kepObj1, kepObj2, kepObj3];
+var balpart = [kepObj1.cim, kepObj2.cim, kepObj3.cim, ];
 var eleresiUt = [];
+var hajo = [];
+var jobbpart = [];
 var id = 0;
+var voltmar;
+var hajohelyzet;
 
 function init() {
     ID("labjegyzet").innerHTML = "Kelemen Kevin, Nagy Domonkos, Labanc Dániel, Joó Edvárd";
     ID("labjegyzet").className = "footerFormaz";
 
-    for (var i = 0; i < kepObjTomb.length; i++) {
+    for (var i = 0; i < KepObjTomb.length; i++) {
         $(".keptarolo img")[i].addEventListener("click", Hozzaad);
 
     }
@@ -29,24 +34,28 @@ function init() {
 
     function Hozzaad() {
         var azon = this.id;
-        
+
         feltolt(azon);
     }
 
 
     function feltolt(index) {
-        eleresiUt.push(kepObjTomb[index].kep);
-        console.log(eleresiUt);
+        eleresiUt.push(KepObjTomb[index].kep);
+        //console.log(eleresiUt);
     }
 
 
-    for (var i = 0; i < $("aside img").length; i++) {
-        $("aside img")[i].addEventListener("mouseover", kiemel);
-        $("aside img")[i].addEventListener("mouseout", levesz);
-        $("aside img")[i].addEventListener("click", berak);
+    for (var i = 0; i < $("#bal img").length; i++) {
+        $("#bal img")[i].addEventListener("mouseover", kiemel);
+        $("#bal img")[i].addEventListener("mouseout", levesz);
+        $("#bal img")[i].addEventListener("click", balrolberak);
         $("article img")[i].addEventListener("click", kirak);
         $("article img")[i].addEventListener("mouseover", kiemel);
         $("article img")[i].addEventListener("mouseout", levesz);
+        $("#jobb img")[i].addEventListener("mouseover", kiemel);
+        $("#jobb img")[i].addEventListener("mouseout", levesz);
+        $("#jobb img")[i].addEventListener("click", jobbrolberak);
+
     }
 
 
@@ -66,15 +75,108 @@ function init() {
         this.classList.remove("kiemel");
     }
 
-    function berak() {
-        this.classList.toggle("nemlatszik");
-        $("article img")[this.id].classList.toggle("nemlatszik");
+    function balrolberak() {
 
+        if (!hiba()) {
+            voltmar = true;
+            var azon = this.id;
+            this.classList.toggle("nemlatszik");
+            $("article img")[azon].classList.toggle("nemlatszik");
+            hajo.push(KepObjTomb[azon].cim);
+            var index = balpart.indexOf(KepObjTomb[azon].cim);
+            balpart.splice(index, 1);
+            hajohelyzet = true;
+        }
+
+        hajoPozicio(hajohelyzet);
+        proba(balpart, !(hajohelyzet));
+        proba(jobbpart, hajohelyzet);
+        console.log(hajohelyzet);
+
+    }
+
+    function jobbrolberak() {
+        if (!hiba()) {
+            voltmar = false;
+            var azon = this.id - 6;
+            this.classList.toggle("nemlatszik");
+            $("article img")[azon].classList.toggle("nemlatszik");
+            hajo.push(KepObjTomb[azon].cim);
+            var index = jobbpart.indexOf(KepObjTomb[azon].cim);
+            jobbpart.splice(index, 1);
+            hajohelyzet = false;
+        }
+
+        hajoPozicio(hajohelyzet);
+        proba(jobbpart, hajohelyzet);
+        proba(balpart, !(hajohelyzet));
+        console.log(hajohelyzet);
     }
 
     function kirak() {
-        this.classList.toggle("nemlatszik");
-        $("aside img")[this.id - 3].classList.toggle("nemlatszik");
+
+
+        var azon = this.id - 3;
+        var index = hajo.indexOf(KepObjTomb[azon].cim);
+        hajo.splice(index, 1);
+
+        if (voltmar) {
+            this.classList.toggle("nemlatszik");
+            $("#jobb img")[azon].classList.toggle("nemlatszik");
+            jobbpart.push(KepObjTomb[azon].cim);
+            hajohelyzet = false;
+        } else {
+            this.classList.toggle("nemlatszik");
+            $("#bal img")[azon].classList.toggle("nemlatszik");
+            balpart.push(KepObjTomb[azon].cim);
+            hajohelyzet = true;
+        }
+
+        hajoPozicio(hajohelyzet);
+        proba(jobbpart, hajohelyzet);
+        proba(balpart, !(hajohelyzet));
+        console.log(hajohelyzet);
     }
+
+    function ellenorzes() {
+        console.log("Bal part elemei: " + balpart);
+        console.log("Hajó elemei: " + hajo);
+        console.log("Jobb part elemei: " + jobbpart);
+    }
+
+
+    function hiba() {
+        var igaz = false;
+        if (hajo.length > 0) {
+            igaz = true;
+            alert("Nem lehet a hajóban a révész mellett 1-nél több karakter!");
+        }
+
+        return igaz;
+    }
+
+    function proba(tomb = [], boolean) {
+        if (tomb.includes(KepObjTomb[0].cim) && tomb.includes(KepObjTomb[2].cim) && !(tomb.includes(KepObjTomb[1].cim)) && boolean) {
+            alert("Van farkas és kecske");
+            window.location.reload();
+        }
+        if (tomb.includes(KepObjTomb[1].cim) && tomb.includes(KepObjTomb[0].cim) && !(tomb.includes(KepObjTomb[2].cim)) && boolean) {
+            alert("Van kecske és káposzta");
+            window.location.reload();
+        }
+        if (tomb.includes(KepObjTomb[1].cim) && tomb.includes(KepObjTomb[0].cim) && tomb.includes(KepObjTomb[2].cim) && !boolean) {
+            alert("NYERT!");
+    }
+    }
+
+    function hajoPozicio(boolean) {
+        if (boolean === true) {
+            ID("csonak").style.backgroundPosition = "left";
+        } else {
+            ID("csonak").style.backgroundPosition = "right";
+        }
+    }
+//ha balról vesszük fel, vagy tesszük le = true
+//ha jobbról vesszük fel, vagy tesszük le = false
 
 }
